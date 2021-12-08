cfcd361fc986203a67edb32767afc3f7ac7604b2
Leonardo Teixeira Cândido <leonardo.leotc@gmail.com>
2021-12-08 20:46:40 -0300

    feat: add controller for automobiles

---
+++ src/controllers/Automobiles.controller.ts
@@ -0,0 +1,65 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+
+import { Automobile } from '../database/entities/Automobiles';
+
+class AutomobileController {
+    async store(req: Request, res: Response) {
+        const repository = getRepository(Automobile);
+
+        const { plate, model, color } = req.body;
+        const automobileExists = await repository.findOne({ where: { plate } });
+
+        if (automobileExists) return res.sendStatus(409);
+
+        const automobile = repository.create({ plate, model, color });
+        await repository.save(automobile);
+
+        return res.json(automobile);
+    }
+
+    async findAll(req: Request, res: Response) {
+        const repository = getRepository(Automobile);
+        const result = await repository.find();
+
+        return res.send(result);
+    }
+
+    async findOne(req: Request, res: Response) {
+        const repository = getRepository(Automobile);
+        const plate: string = req.params.plate as string;
+
+        const result = await repository.find({ where: { plate } });
+        if (!result)
+            return res.status(404).json({ mensagem: `Automobile not found` });
+
+        return res.status(200).send(result);
+    }
+
+    async update(req: Request, res: Response) {
+        const repository = getRepository(Automobile);
+        const { model, color } = req.body;
+        const plate: string = req.params.plate;
+
+        const automobilesExists = await repository.findOne({
+            where: { plate },
+        });
+
+        automobilesExists!.model = model;
+        automobilesExists!.color = color;
+
+        await repository.save(automobilesExists!);
+
+        return res.json(automobilesExists);
+    }
+
+    async delete(req: Request, res: Response) {
+        const repository = getRepository(Automobile);
+        const plate: string = req.params.plate as string;
+        await repository.delete(plate);
+
+        return res.send('Automobile Deleted');
+    }
+}
+
+export default new AutomobileController();
