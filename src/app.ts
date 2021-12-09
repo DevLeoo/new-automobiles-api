e263451f8bf3a4bd6de0872f2067bd685b3d2c0c
Leonardo Teixeira Cândido <leonardo.leotc@gmail.com>
2021-12-08 22:31:40 -0300

    feat: add swagger ui express

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,8 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
-
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocs from './swagger.json';
 import routes from './routes';
 
 class App {
@@ -19,6 +20,7 @@ class App {
         this.app.use(cors());
         this.app.use(bodyParser.json({ limit: '50mb' }));
         this.app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }));
+        this.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
         this.app.use(routes);
         this.app.disable('x-powered-by');
     }
