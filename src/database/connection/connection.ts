a467c81cd6670034ff4a7b53f018519ff6bbed01
Leonardo Teixeira Cândido <leonardo.leotc@gmail.com>
2021-12-09 13:26:31 -0300

    fix: changed method for create only one connection

--- src/database/connection/connection.ts
+++ src/database/connection/connection.ts
@@ -1,16 +1,13 @@
 import path from 'path';
-import dotenv from 'dotenv';
-import { createConnections } from 'typeorm';
+import { createConnection } from 'typeorm';
 
-createConnections([
-    {
-        type: 'mysql',
-        host: process.env.DB_HOST,
-        port: +process.env.DB_PORT!,
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        entities: [`${path.resolve(__dirname, '../entities')}/*.{ts,js}`],
-        subscribers: [`${path.resolve(__dirname, '../../subscriber')}/*.{ts,js}`],
-    },
-]).then(() => console.log('📦 Connected with database'));
+createConnection({
+    type: 'mysql',
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT!,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [`${path.resolve(__dirname, '../entities')}/*.{ts,js}`],
+    subscribers: [`${path.resolve(__dirname, '../../subscriber')}/*.{ts,js}`],
+}).then(() => console.log('📦 Connected with database'));
