4021cd5c18d983a2d41e475dfe32681f20e9da4c
Leonardo Teixeira Cândido <leonardo.leotc@gmail.com>
2021-12-09 21:18:33 -0300

    fix: changed methods to use repository functions

--- src/controllers/Drivers.controllers.ts
+++ src/controllers/Drivers.controllers.ts
@@ -1,59 +1,52 @@
 import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
+import {
+    findById,
+    save,
+    findAll,
+    update,
+    remove,
+} from '../repository/drivers.repository';
 
 import { Driver } from '../database/entities/Drivers';
 
 class DriverController {
     async store(req: Request, res: Response) {
-        const repository = getRepository(Driver);
-
         const { id, name } = req.body;
-        const driverExists = await repository.findOne({ where: { id } });
+        const driverExists = await findById(id);
 
         if (driverExists) return res.sendStatus(409);
 
-        const driver = repository.create({ id, name });
-        await repository.save(driver);
-
-        return res.json(driver);
+        return res.json(await save({ name }));
     }
 
-    async findAll(req: Request, res: Response) {
-        const repository = getRepository(Driver);
-        const result = await repository.find();
-
-        return res.send(result);
+    async find(req: Request, res: Response) {
+        return res.send(await findAll());
     }
 
     async findOne(req: Request, res: Response) {
-        const repository = getRepository(Driver);
         const id: string = req.params.id as string;
 
-        const result = await repository.findOne({ where: { id } });
+        const result = await findById(id);
         if (!result) return res.status(404).json({ mensagem: `Driver not found` });
 
         return res.status(200).send(result);
     }
 
     async update(req: Request, res: Response) {
-        const repository = getRepository(Driver);
         const id: string = req.params.id;
         const { name } = req.body;
 
-        const driver = await repository.findOne({
-            where: { id },
-        });
+        const driver = await findById(id);
 
         driver!.name = name;
-        await repository.save(driver!);
+        await update(driver);
 
         return res.json(driver);
     }
 
     async delete(req: Request, res: Response) {
-        const repository = getRepository(Driver);
         const id: string = req.params.id as string;
-        const resp = await repository.delete(id);
+        const resp = await remove(id);
         if (resp.affected == 0)
             return res.status(404).json({ mensagem: 'Driver not found' });
         return res.send('Driver Deleted');
