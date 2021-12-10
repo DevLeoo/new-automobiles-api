265c1fb4b2b14d09845096d043cd578e19c40605
Leonardo Teixeira Cândido <leonardo.leotc@gmail.com>
2021-12-09 21:28:05 -0300

    fix: changed controllers methods to use repository methods

--- src/controllers/Rent.controller.ts
+++ src/controllers/Rent.controller.ts
@@ -1,12 +1,10 @@
 import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
+import { findById, save, findAll, update } from '../repository/rent.repository';
+
 import validateAll from '../utils/checkRent';
-import { Rent } from '../database/entities/Rent';
 
 class RentController {
     async store(req: Request, res: Response) {
-        const repository = getRepository(Rent);
-
         const { driver, automobile, delivery_date } = req.body;
 
         if (!(await validateAll(driver, automobile)))
@@ -14,37 +12,30 @@ class RentController {
                 mensagem: 'Driver and/or Automobile are unavailable or not exist',
             });
 
-        const rent = repository.create({
+        const rent = save({
             driver,
             automobile,
             withdrawal_date: new Date().toDateString(),
             delivery_date,
         });
-        await repository.save(rent);
 
         return res.json(rent);
     }
 
-    async findAll(req: Request, res: Response) {
-        const repository = getRepository(Rent);
-        const result = await repository.find();
-
-        return res.send(result);
+    async find(req: Request, res: Response) {
+        return res.send(await findAll());
     }
 
     async finish(req: Request, res: Response) {
-        const repository = getRepository(Rent);
         const id: string = req.params.id;
 
-        const rent = await repository.findOne({
-            where: { id },
-        });
+        const rent = await findById(id);
 
         if (!rent) return res.status(404).json({ mensagem: 'Rent not found' });
 
         rent!.delivery_date = new Date().toDateString() as any;
 
-        await repository.save(rent!);
+        await update(rent);
 
         return res.json(rent);
     }
