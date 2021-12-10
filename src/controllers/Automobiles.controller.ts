35f7c532d7d10b0cf1d5a37274f82585700bbfc9
Leonardo Teixeira Cândido <leonardo.leotc@gmail.com>
2021-12-09 21:10:08 -0300

    fix: change method, now is using repository methods

--- src/controllers/Automobiles.controller.ts
+++ src/controllers/Automobiles.controller.ts
@@ -1,35 +1,30 @@
 import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
-
-import { Automobile } from '../database/entities/Automobiles';
+import {
+    findByPlate,
+    save,
+    findAll,
+    update,
+    remove,
+} from '../repository/automobiles.repository';
 
 class AutomobileController {
     async store(req: Request, res: Response) {
-        const repository = getRepository(Automobile);
-
         const { plate, model, color } = req.body;
-        const automobileExists = await repository.findOne({ where: { plate } });
+        const automobileExists = await findByPlate(plate);
 
         if (automobileExists) return res.sendStatus(409);
-
-        const automobile = repository.create({ plate, model, color });
-        await repository.save(automobile);
-
+        const automobile = save({ plate, model, color });
         return res.json(automobile);
     }
 
-    async findAll(req: Request, res: Response) {
-        const repository = getRepository(Automobile);
-        const result = await repository.find();
-
-        return res.send(result);
+    async find(req: Request, res: Response) {
+        return res.send(await findAll());
     }
 
     async findOne(req: Request, res: Response) {
-        const repository = getRepository(Automobile);
         const plate: string = req.params.plate as string;
 
-        const result = await repository.findOne({ where: { plate } });
+        const result = await findByPlate(plate);
 
         if (!result)
             return res.status(404).json({ mensagem: `Automobile not found` });
@@ -38,28 +33,26 @@ class AutomobileController {
     }
 
     async update(req: Request, res: Response) {
-        const repository = getRepository(Automobile);
         const { model, color } = req.body;
         const plate: string = req.params.plate;
 
-        const automobilesExists = await repository.findOne({
-            where: { plate },
-        });
+        const automobilesExists = await findByPlate(plate);
+
         if (!automobilesExists)
             return res.status(404).json({ mensagem: 'Automobile not found' });
 
         automobilesExists!.model = model;
         automobilesExists!.color = color;
 
-        await repository.save(automobilesExists!);
+        await update(automobilesExists);
 
         return res.json(automobilesExists);
     }
 
     async delete(req: Request, res: Response) {
-        const repository = getRepository(Automobile);
         const plate: string = req.params.plate as string;
-        const resp = await repository.delete(plate);
+        const resp = await remove(plate);
+
         if (resp.affected == 0)
             return res.status(404).json({ mensagem: 'Automobile not found' });
         return res.send('Automobile Deleted');
